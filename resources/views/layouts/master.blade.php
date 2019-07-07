31bf6b9283dbb7b853c4fde94de7064765c9e25b
oneblood39 <birkanyanar@gmail.com>
2019-07-07 13:18:27 +0300

    renkli ikonlar

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -78,7 +78,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                with font-awesome or any other icon font library -->
                  <li class="nav-item">
                    <router-link to="/dashboard" class="nav-link">
-                     <i class="nnav-icon fas fa-tachometer-alt"></i>
+                     <i class="nnav-icon fas fa-tachometer-alt blue"></i>
                      <p>
                        Dashboard
                       <!-- <span class="right badge badge-danger">New</span> -->
@@ -88,7 +88,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
           <li class="nav-item has-treeview">
             <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-cog"></i>
+              <i class="nav-icon fas fa-cog green"></i>
               <p>
                 Management
                 <i class="right fas fa-angle-left"></i>
@@ -111,19 +111,25 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </li>
           <li class="nav-item">
             <router-link to="/profile" class="nav-link">
-              <i class="nnav-icon fas fa-user"></i>
+              <i class="nnav-icon fas fa-user orange"></i>
               <p>
                 Profile
               </p>
             </a>
           </li>
           <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nnav-icon fas fa-power-off"></i>
-              <p>
-                Log Out
-              </p>
+
+
+            <a class="nav-link" href="{{ route('logout') }}"
+               onclick="event.preventDefault();
+                             document.getElementById('logout-form').submit();">
+                             <i class="nnav-icon fas fa-power-off red"></i>
+                <p>{{ __('Logout') }}</p>
             </a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
           </li>
 
         </ul>
